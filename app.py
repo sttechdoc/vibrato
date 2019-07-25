1c14bc762dc8db7676c6a709f429b2a8c8a728de
sttechdoc <srav.vuppalapati@gmail.com>
2019-07-25 20:50:58 +1000

    docker compose and web page changes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def test_project():
-    return "WELCOME TO THE PROJECT..."
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(8080), debug=False)
